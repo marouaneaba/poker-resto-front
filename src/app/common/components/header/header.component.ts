f31f0a9161ec902376907e2ae008ad6d52a7c4f4
marouaneab <mar.abakarim@gmail.com>
2021-05-28 03:40:57 +0200

    e

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  protected imageHeaderDisplay: string;
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  public onChangeNavBarImage(event) {
+    this.imageHeaderDisplay = event;
+  }
+
+  public getBackgroungImageStyle(): Object {
+    switch (this.imageHeaderDisplay) {
+      case '/coffee':
+        return {"background-image": "URL(\"/assets/images/header/coffee/coffee_1.png\")"};
+      case '/bar':
+        return {"background-image": "URL(\"/assets/images/header/bar/bar_2.png\")"};
+      case '/restaurant':
+        return {"background-image": "URL(\"/assets/images/header/restaurant/restaurant_2.png\")"};
+      case '/beer':
+        return {"background-image": "URL(\"/assets/images/header/beer/beer_3.png\")"};
+      default:
+        return {"background-image": "URL(\"/assets/images/header/index-image.png\")"};
+    }
   }
 
 }
